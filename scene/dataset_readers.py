b8c8969a1a4a35cc9eb3230b20e2b116c5b28b9b
MCYang273730 <75789605+MCYang273730@users.noreply.github.com>
2023-12-30 23:58:14 +0800

    Allow reading point clouds without colors or normals

--- scene/dataset_readers.py
+++ scene/dataset_readers.py
@@ -108,8 +108,16 @@ def fetchPly(path):
     plydata = PlyData.read(path)
     vertices = plydata['vertex']
     positions = np.vstack([vertices['x'], vertices['y'], vertices['z']]).T
-    colors = np.vstack([vertices['red'], vertices['green'], vertices['blue']]).T / 255.0
-    normals = np.vstack([vertices['nx'], vertices['ny'], vertices['nz']]).T
+    try:
+        colors = np.vstack([vertices['red'], vertices['green'], vertices['blue']]).T / 255.0
+    except ValueError:
+        warn(f'No color information in {path!r}, generating random colors.')
+        shs = np.random.random((len(vertices), 3)) / 255.0
+        colors = SH2RGB(shs)
+    try:
+        normals = np.vstack([vertices['nx'], vertices['ny'], vertices['nz']]).T
+    except ValueError:
+        normals = np.zeros_like(positions)
     return BasicPointCloud(points=positions, colors=colors, normals=normals)
 
 def storePly(path, xyz, rgb):
